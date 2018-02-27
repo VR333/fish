76b222830882c6eff8f2bfdf290eca751969757a
Rostyslav Vakh <rvakh@softserveinc.com>
2018-02-27 17:50:47 +0200

    add main and mune component, try header tooltips

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ForbiddenComponent } from './forbidden/app.component';
 import { HeaderComponent } from './header/app.component';
-
+import { MenuComponent } from './menu/app.component';
+import { MainContentComponent } from './main/app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ForbiddenComponent
+    ForbiddenComponent,
+    MenuComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule
