f84a974dbf4077fe9995d59e42f1b9ab4fbc2b22
Rostyslav Vakh <rvakh@softserveinc.com>
2018-03-05 16:46:46 +0200

    edit menu component

--- src/app/menu/app.component.ts
+++ src/app/menu/app.component.ts
@@ -6,5 +6,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class MenuComponent {
+    showExtendedMenu:boolean = false;
+    showExtendedCategory:boolean = false;
 
+    coreMenuOptions = [
+        {
+            text: 'Income'
+        },
+        {
+            text: 'Starred'
+        },
+        {
+            text: 'Sent'
+        },
+        {
+            text: 'Draft'
+        }
+    ];
+
+    extendedMenuOptions = [
+        {
+            text: 'Important'
+        },
+        {
+            text: 'Chats'
+        },
+        {
+            text: 'All'
+        },
+        {
+            text: 'Spam'
+        },
+        {
+            text: 'Basket'
+        }
+    ];
+
+    extendedCategoryOptions = [
+        {
+            text: 'IMG Social network'
+        },
+        {
+            text: 'IMG Advertisment'
+        },
+        {
+            text: 'IMG Updates'
+        },
+        {
+            text: 'IMG Forums'
+        }
+    ];
+
+    toShowExtendedMenu() {
+        this.showExtendedMenu = !this.showExtendedMenu;
+    }
+
+    toShowExtendedCategory() {
+        this.showExtendedCategory = !this.showExtendedCategory;
+    }
 }
