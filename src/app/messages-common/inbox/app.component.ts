d60fd575cd02632674892a9c5bd22f10c13643ff
Rostyslav Vakh <rvakh@softserveinc.com>
2018-03-14 15:42:11 +0200

    add @media to main and menu, plus inbox message category generates dynamicly....still need to fix active color for inbox message category

--- src/app/messages-common/inbox/app.component.ts
+++ src/app/messages-common/inbox/app.component.ts
@@ -9,6 +9,30 @@ import { ManageMessagesService } from './../../services/manageMessage';
 })
 export class InboxComponent implements OnInit, DoCheck {
     constructor(private msg: ManageMessagesService) {}
+    categoryButtonActive: number = 0;
+    categoryButtons = [
+        {
+            id: 0,
+            class: 'category-common category grey jump-fix',
+            color: 'active-grey',
+            icon: 'fas fa-inbox',
+            text: 'Основні'
+        },
+        {
+            id: 1,
+            class: 'category-common category blue jump-fix',
+            color: 'active-blue',
+            icon: 'fas fa-users',
+            text: 'Соцмережі'
+        },
+        {
+            id: 2,
+            class: 'category-common category green jump-fix',
+            color: 'active-green',
+            icon: 'fas fa-tags',
+            text: 'Реклама'
+        }
+    ];
 
     messages = MESSAGES.filter( message => message.category === 'income');
 
@@ -19,5 +43,9 @@ export class InboxComponent implements OnInit, DoCheck {
 
     ngDoCheck() {
         this.messages = MESSAGES.filter( message => message.category === 'income');
+    }
+
+    makeCategoryActive(categoryId) {
+        this.categoryButtonActive = categoryId;
     }
 }
