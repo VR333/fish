91089c56ceceb483ee8164b9f499b7975f97faee
Rostyslav Vakh <rvakh@softserveinc.com>
2018-03-15 18:23:37 +0200

    fix creatChat btn inside user-profile component, fix main component height

--- src/app/menu/user-profile/app.component.ts
+++ src/app/menu/user-profile/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class UserProfileComponent {
     showChatCreation:boolean = false;
+
+    createChat() {
+        this.showChatCreation = !this.showChatCreation;
+    }
 }
