cbb08bfa73dbbcb74c3330f5b6d0e358de8dbce9
Rostyslav Vakh <rvakh@softserveinc.com>
2018-04-04 11:41:35 +0300

    add send message* functionality, fix tooltip z-index --- not a final implementation

--- src/app/new-messages/app.component.ts
+++ src/app/new-messages/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NewMessageService } from './../services/NewMessageService';
 
 @Component({
@@ -7,10 +7,35 @@ import { NewMessageService } from './../services/NewMessageService';
   styleUrls: ['./app.component.scss']
 })
 export class NewMessagesComponent {
+    @ViewChild('msgTitle') msgTitle;
+    @ViewChild('msgTopic') msgTopic;
+    @ViewChild('msgContent') msgContent;
+
     constructor(private NewMessageService: NewMessageService) {}
     newMessages = this.NewMessageService.newMessages;
 
     deleteTemplate(message) {
         this.NewMessageService.deleteMessageTemplate(message);
+    }
+
+    sendNewMessage(newMessage) {
+        let title = this.msgTitle.nativeElement.value;
+        let content = this.msgContent.nativeElement.value;
+        let topic = this.msgTopic.nativeElement.value;
+
+        let warningMessage = "Надіслати це повідомлення без теми і тексту?";
+        let isReceiverSet = !!title;
+        let isTopicOrContentSet = !!content || !!topic;
+
+        if (isReceiverSet) {
+
+            if (isTopicOrContentSet || confirm(warningMessage) ) {
+                this.NewMessageService.sendMessage(title, topic, content);
+                this.NewMessageService.deleteMessageTemplate(newMessage);
+            }
+
+        } else {
+            alert("Укажіть хоча б одного адресата..");
+        }
     }
 }
--- src/app/services/NewMessageService.ts
+++ src/app/services/NewMessageService.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
+import { MESSAGES } from './../messages-common/messages';
 
 @Injectable()
 export class NewMessageService {
 	newMessages = [];
+	messages = MESSAGES;
 
 	createMessageTemplate() {
 		this.newMessages.push({});
@@ -12,4 +14,38 @@ export class NewMessageService {
 		let index = this.newMessages.indexOf(message);
 		this.newMessages.splice(index, 1);
 	}
+
+	sendMessage(title, topic, content) {
+		this.messages.push({
+			id: this.messages.length,
+			active: false,
+			title: title,
+			content: content,
+			date: this.getCurrentDate(),
+			clip: false,
+			type: 'sent',
+			starred: false,
+			read: true,
+			important: false,
+			category: 'main'
+		});
+	}
+
+	getCurrentDate() {
+		return new Date().toLocaleDateString('ua');
+	}
 }
+
+// {
+// 	id: 0,
+// 	active: false,
+// 	title: 'Fish Site',
+// 	content: 'Hi, my name is Rostyslavfgdfghdfghdfgfghdfghfghdfghhdfghdgfhgfhdfghdfgh',
+// 	date: '07.03.18',
+// 	clip: true,
+// 	type: 'income',
+// 	starred: true,
+// 	read: true,
+// 	important: false,
+// 	category: 'main'
+// },
